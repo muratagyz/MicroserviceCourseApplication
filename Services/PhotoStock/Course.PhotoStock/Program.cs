16e2d1601dd3c27ccf95a57a1fb6ade32f11e95a
Murat AĞYÜZ <57285600+muratagyz@users.noreply.github.com>
2023-11-21 14:59:23 +0300

    Finish

---
+++ Services/PhotoStock/Course.PhotoStock/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration.GetRequiredSection("IdentityServerURL").Value;
+    options.Audience = "resource_photo_stock";
+    options.RequireHttpsMetadata = false;
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
