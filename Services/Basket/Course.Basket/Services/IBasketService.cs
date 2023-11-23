a982158120f6dbcec9a91ea7417818c235bc83af
Murat AĞYÜZ <57285600+muratagyz@users.noreply.github.com>
2023-11-23 15:44:50 +0300

    Part 1

---
+++ Services/Basket/Course.Basket/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using Course.Basket.Dtos;
+using Course.Shared.Dtos;
+
+namespace Course.Basket.Services;
+
+public interface IBasketService
+{
+    Task<Response<BasketDto>> GetBasket(string userId);
+    Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+    Task<Response<bool>> Delete(string userId);
+}
---
+++ Services/Basket/Course.Basket/Settings/RedisSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Course.Basket.Settings;
+
+public class RedisSettings
+{
+    public string Host { get; set; }
+    public int Port { get; set; }
+}
