7812eb7c46db12d95a0ae83a2aa0aa692547b2fd
Murat AĞYÜZ <57285600+muratagyz@users.noreply.github.com>
2023-11-27 22:14:10 +0300

    Part 2

--- Services/Basket/Course.Basket/Program.cs
+++ Services/Basket/Course.Basket/Program.cs
@@ -1,27 +1,48 @@
 using Course.Basket.Services;
 using Course.Basket.Settings;
+using Course.Shared.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var requreAuthorizePlociy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration.GetRequiredSection("IdentityServerURL").Value;
+    options.Audience = "resource_basket";
+    options.RequireHttpsMetadata = false;
+});
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requreAuthorizePlociy));
+});
+
 builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
-
 // Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+builder.Services.AddScoped<IBasketService, BasketService>();    
 builder.Services.AddSingleton<RedisService>(sp =>
 {
     var redisSettings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
 
     var redis = new RedisService(redisSettings.Host, redisSettings.Port);
 
-    redis.Connect();    
+    redis.Connect();
 
-    return redis;   
+    return redis;
 });
 
 var app = builder.Build();
@@ -34,6 +55,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
